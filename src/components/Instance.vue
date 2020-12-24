f310d96f3b76858b9c71e930cf2e77278562670d
Ry <Rylius@users.noreply.github.com>
2020-12-24 16:56:45 +0100

    Added SearchForDataContainer shortcut to instances with a name set

--- src/components/Instance.vue
+++ src/components/Instance.vue
@@ -9,6 +9,24 @@
             <button class="button is-small" v-else @click="visible = true">Show</button>
         </h4>
 
+        <div class="field is-horizontal" v-if="instance.fields.name && instance.fields.name.value">
+            <div class="field-label">
+                <label class="is-family-code" :for="`lua-SearchForDataContainer-${instance.guid}`">
+                    SearchForDataContainer
+                </label>
+            </div>
+
+            <div class="field-body field has-addons">
+                <input type="text" class="input control is-small is-family-code" readonly
+                       :value="luaSearchForDataContainer(instance)"
+                       :id="`lua-SearchForDataContainer-${instance.guid}`">
+                <button class="button control is-small"
+                        @click="copyToClipboard(luaSearchForDataContainer(instance))">
+                    Copy
+                </button>
+            </div>
+        </div>
+
         <div class="field is-horizontal">
             <div class="field-label">
                 <label class="is-family-code" :for="`lua-FindInstanceByGuid-${instance.guid}`">
@@ -110,6 +128,9 @@ export default Vue.extend({
     methods: {
         luaFindInstanceByGuid(partition: Partition, instance: Instance) {
             return `${instance.type}(ResourceManager:FindInstanceByGuid(Guid('${partition.guid}'), Guid('${instance.guid}')))`;
+        },
+        luaSearchForDataContainer(instance: Instance) {
+            return `${instance.type}(ResourceManager:SearchForDataContainer('${instance.fields.name.value}'))`;
         },
         luaFindInstance(instance: Instance) {
             return `${instance.type}(partition:FindInstance(Guid('${instance.guid}')))`;
